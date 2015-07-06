f55819cd35c09d352f20e9860369ef830775534f
knjname <pc@knj.name>
2015-07-06 20:07:15 +0900

    upgrade Clojure to the latest version.

--- resources/leiningen/new/flat_layout/project.clj
+++ resources/leiningen/new/flat_layout/project.clj
@@ -1,7 +1,7 @@
 (defproject {{name}} "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]]
   :source-paths ["."]
   :repl-options {:init-ns {{name}}}
   :main {{name}})
